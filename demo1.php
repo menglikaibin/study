61057eae9e2610155227a516029b51cc7bfea02a
wukaibin <wukaibin@linhuiba.com>
2020-07-06 17:00:50 +0800

    依赖注入:

--- demo1.php
+++ demo1.php
@@ -24,24 +24,35 @@ class NastyBoss
 {
     private $employees = [];
 
-    public function addEmployee(string $employeeName)
+    public function addEmployee(Employee $employee)
     {
         // 每个员工都是一个对象,拥有Minion和Employee的方法和属性
-        $this->employees[] = new Minion($employeeName);
+        $this->employees[] = $employee;
     }
 
     public function projectFailed()
     {
         if (count($this->employees) > 0) {
-            // 抛出每一个员工,执行fire方法
+            // 抛出第一个员工,执行fire方法
             $emp = array_pop($this->employees);
             $emp->fire();
         }
     }
 }
 
+class CluedUp extends Employee
+{
+    public function fire()
+    {
+        print "{$this->name}: I'll call my lawyer\n";
+    }
+}
+
 // 运行
 $boss = new NastyBoss();
-$boss->addEmployee('harry');
-$boss->addEmployee('bob');
+$boss->addEmployee(new Minion('harry'));
+$boss->addEmployee(new Minion('bob'));
+$boss->addEmployee(new CluedUp('mary'));
+$boss->projectFailed();
+$boss->projectFailed();
 $boss->projectFailed();
